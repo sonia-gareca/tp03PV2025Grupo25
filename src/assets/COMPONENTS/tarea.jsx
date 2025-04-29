f9756017592cba0e73042fc168384e7886985238
joaco2022 <fulano2200@gmail.com>
2025-04-29 19:31:48 -0300

    Agregando producto.jsx y tarea.jsx por Calermo Joaquin

---
+++ src/assets/COMPONENTS/tarea.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import '../CSS/tarea.css';
+
+function Tarea() {
+    const [tareas, setTareas] = useState([]);
+    const [nuevaTarea, setNuevaTarea] = useState('');
+
+    const agregarTarea = () => {
+        if (nuevaTarea.trim() !== '') {
+        setTareas([...tareas, { texto: nuevaTarea, completada: false }]);
+        setNuevaTarea('');
+    }
+};
+
+    const eliminarTarea = (index) => {
+        const nuevasTareas = tareas.filter((_, i) => i !== index);
+        setTareas(nuevasTareas);
+    };
+
+    const completarTarea = (index) => {
+    const nuevasTareas = tareas.map((tarea, i) =>
+    i === index ? { ...tarea, completada: !tarea.completada } : tarea
+    );
+    setTareas(nuevasTareas);
+};
+return (
+    <div className="tarea">
+        <h2>Lista de Tareas</h2>
+        <input
+            type="text"
+            value={nuevaTarea}
+            onChange={(e) => setNuevaTarea(e.target.value)}
+            placeholder="Nueva tarea"
+        />
+        <button onClick={agregarTarea}>Agregar</button>
+
+        <ul>
+        {tareas.map((tarea, index) => (
+            <li key={index}>
+                <span
+                style={{ textDecoration: tarea.completada ? 'line-through' : 'none' }}
+                onClick={() => completarTarea(index)}
+                >
+                {tarea.texto}
+            </span>
+            <button onClick={() => eliminarTarea(index)}>Eliminar</button>
+        </li>
+        ))}
+    </ul>
+</div>
+);
+}
+
+export default Tarea;
