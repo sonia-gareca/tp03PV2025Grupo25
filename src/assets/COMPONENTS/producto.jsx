604ee0dc2706d9f78d2339ae4c2c8b249e41eec1
Erni97 <farfan.ernesto14@gmail.com>
2025-05-04 16:42:04 -0300

    traido producto.jsx desde antonioAlmada

--- src/assets/COMPONENTS/producto.jsx
+++ src/assets/COMPONENTS/producto.jsx
@@ -1,37 +1,14 @@
-import '../CSS/producto.css';
+import '../CSS/estilos.css';
 
 function Producto() {
-    const [productos, setProductos] = useState([
-        { nombre: 'Monitor', categoria: 'Electrónica' },
-        { nombre: 'Teclado', categoria: 'Electrónica' },
-        { nombre: 'Mouse', categoria: 'Electrónica' },
-        { nombre: 'Notebook', categoria: 'Electrónica' },
-        { nombre: 'Auriculares', categoria: 'Accesorios' },
-    ]);
-    const [filtro, setFiltro] = useState('todas'); // Estado para el filtro
-
-    const filtrarProductos = () => { //se agrega la función filtrarProductos
-        if (filtro === 'Electrónica') {
-            return productos.filter((producto) => producto.categoria === 'Electrónica');
-        } else if (filtro === 'Accesorios') {
-            return productos.filter((producto) => producto.categoria === 'Accesorios');
-        }
-        return productos; // Si el filtro es "todas", devuelve todos los productos
-    };
+    const productos = ['Monitor', 'Teclado', 'Mouse', 'Notebook', 'Auriculares'];
 
     return (
         <div className="producto">
             <h2>Lista de Productos</h2>
-
-            <div className="filtros">
-                <button onClick={() => setFiltro('todas')}>Todas</button>
-                <button onClick={() => setFiltro('Electrónica')}>Electrónica</button>
-                <button onClick={() => setFiltro('Accesorios')}>Accesorios</button>
-            </div>
-
             <ul>
-                {filtrarProductos().map((prod, index) => (
-                    <li key={index}>{prod.nombre}</li>
+                {productos.map((prod, index) => (
+                    <li key={index}>{prod}</li>
                 ))}
             </ul>
         </div>
@@ -39,5 +16,4 @@ function Producto() {
     
     );
 }
-
 export default Producto;
